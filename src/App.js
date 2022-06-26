738916671c51ef9ff5319778e247328103868fc1
Divyam <divyamsharma822@gmail.com>
2022-06-26 18:33:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Card from './components/Card/Card';
 const App = () => {
     return (
       <div className="App">
-        <Card/>
-
+        <Card 
+        img={"https://images.pexels.com/photos/7858744/pexels-photo-7858744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'"}
+        title={"Nord Stage 3 88 Keys"}
+        brandNew={"Brand New"}
+        shipping={"Free Shipping"}
+        offer={"15% "}
+        />
       </div>
     );
   }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import "./Card.scss";
 
-const Card = ({ emoji, heading, detail }) => {
+const Card = ({img, title, brandNew, shipping, offer }) => {
     return (
-       <div className="card">
-        <div className="hi"
-       </div>
+        <div className='card'>
+            <div className='card-left'>
+                <img
+                    src={img}
+                    alt=''
+                />
+                <div className='img-text'>
+                    {/* {imageText} */}
+                    <span>{title}</span>
+                    <div className='tags'>
+                        { brandNew && <span className='brand-new'>{ brandNew }</span> }
+                        { shipping && <span className='shipping'>{ shipping }</span> }
+                        { offer && <span className='offer'>{ offer }% Off</span>}
+                    </div>
+                </div>
+            </div>
+            <div className='card-right'>
+                <div className='left'>
+                    <div className='company'>E.M.L.A</div>
+                    <div className='location'>LeHvare, France</div>
+                    <div className='review'>⭐️⭐️⭐️⭐️⭐️(23)</div>
+                </div>
+                <div className='middle'>
+                    <div className="price-cut">$6500</div>
+                    <div className="new-price">$3433 </div>
+                    <div className="plus-shipping">+195Shipping</div>
+                </div>
+                <div className='right'>
+                    <button>Add To Cart</button>
+                    <button>Make Offer</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
