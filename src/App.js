02895b386237f14fd63375c4a2f576cdd457499d
Divyam <divyamsharma822@gmail.com>
2022-06-26 17:31:34 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Card from './components/Card/Card';
+
+const App = () => {
+    return (
+      <div className="App">
+        <Card/>
+
+      </div>
+    );
+  }
+  
+  export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.scss";
+
+const Card = ({ emoji, heading, detail }) => {
+    return (
+       <div className="card">
+        <div className="hi"
+       </div>
+    );
+};
+
+export default Card;
